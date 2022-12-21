f5aebdf8011016289852e3d1513a0924d1cde094
anushaAnu333 <97461362+anushaAnu333@users.noreply.github.com>
2022-12-21 18:25:53 +0530

    initial commit

--- client/src/components/Router.jsx
+++ client/src/components/Router.jsx
@@ -9,8 +9,14 @@ const Router = () => {
 		<div>
 			<Routes>
 			<Route path="/">
-                  <Route index element={<HomePage />} />
-                  <Route path="signin" element={<SignIn />} />
+                  <Route index element={<HomePage type="random" />} />
+                  <Route path="trends" element={<HomePage type="trend" />} />
+                  <Route path="subscriptions" element={<HomePage type="sub" />} />
+                  <Route path="search" element={<Search />} />
+                  <Route
+                    path="signin"
+                    element={currentUser ? <Home /> : <SignIn />}
+                  />
                   <Route path="video">
                     <Route path=":id" element={<VideoPage />} />
                   </Route>
