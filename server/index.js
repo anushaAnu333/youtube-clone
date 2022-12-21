8ee37e40092c5411d344b32af6d95466c8698601
anushaAnu333 <97461362+anushaAnu333@users.noreply.github.com>
2022-12-21 10:19:22 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,26 +1,6 @@
-<<<<<<< HEAD
-const express=require("express")
-const mongoose=require("mongoose")
-const app=express()
-require("dotenv").config()
-const connect=()=>{
-    mongoose.connect(process.env.MONGO_URL)
-    .then(()=>{
-        console.log("connected to db")
-    })
- .catch((err)=>{
-    throw err
- })
-}
 
 
 
-
-app.listen(8880,()=>{
-    connect()
-    console.log("connected to server")
-})
-=======
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
@@ -44,4 +24,4 @@ app.listen(8380, async () => {
 	}
 	console.log("connected to server");
 });
->>>>>>> 86c5ccc0d467ce5501fd97acfe8f58b991836375
+
